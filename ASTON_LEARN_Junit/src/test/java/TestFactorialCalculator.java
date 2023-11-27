d062ad6e7d182fdc74375ee0f643951f28b28df3
karamelev <karamelev@tut.by>
2023-11-27 22:05:47 +0300

    Lesson_12_Junit

---
+++ ASTON_LEARN_Junit/src/test/java/TestFactorialCalculator.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestFactorialCalculator {
+        FactorialCalculator calculator = new FactorialCalculator();
+
+    @ParameterizedTest
+    @CsvSource (value = {
+            "0,1",
+            "1,1",
+            "5,120",
+            "10,3628800"
+    })
+    public void testFactorialCalculator (int input, int result) {
+        Assertions.assertEquals(result,calculator.factorialCalculator(input));
+    }
+
+    @Test()
+    public void testFactorialCalculatorForNegativeNumber () {
+        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> calculator.factorialCalculator(-5));
+        assertEquals(exception.getMessage(),"Факториал отрицательного числа не определен");
+    }
+}
