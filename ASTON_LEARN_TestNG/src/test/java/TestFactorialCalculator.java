512f8c295821e3b5870cbc034b9d8dc3fcef1dea
karamelev <karamelev@tut.by>
2023-11-28 11:34:31 +0300

    Lesson_12_Junit

--- ASTON_LEARN_TestNG/src/test/java/TestFactorialCalculator.java
+++ ASTON_LEARN_TestNG/src/test/java/TestFactorialCalculator.java
@@ -1,17 +1,18 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertThrows;
+
 
 public class TestFactorialCalculator {
     FactorialCalculator calculator = new FactorialCalculator();
     @Test (dataProvider = "factorialCalculator",dataProviderClass = TestData.class)
     public void testFactorialCalculator (int input, int result) {
-        Assert.assertEquals(result, calculator.factorialCalculator(input));
-    }
+        if (input>=0) {
+            Assert.assertEquals(result, calculator.factorialCalculator(input));
+        } else {
+            assertThrows(IllegalArgumentException.class, ()->calculator.factorialCalculator(input));
 
-    @Test (expectedExceptions = IllegalArgumentException.class)
-    public void testFactorialCalculatorForNegativeNumber () {
-         calculator.factorialCalculator(-5);
-
+        }
     }
 }
