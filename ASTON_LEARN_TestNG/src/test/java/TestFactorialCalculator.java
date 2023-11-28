66e81b224772505d02ad0f525208fd8c86c628af
karamelev <karamelev@tut.by>
2023-11-28 11:35:32 +0300

    Lesson_12_Junit

--- ASTON_LEARN_TestNG/src/test/java/TestFactorialCalculator.java
+++ ASTON_LEARN_TestNG/src/test/java/TestFactorialCalculator.java
@@ -12,7 +12,6 @@ public class TestFactorialCalculator {
             Assert.assertEquals(result, calculator.factorialCalculator(input));
         } else {
             assertThrows(IllegalArgumentException.class, ()->calculator.factorialCalculator(input));
-
         }
     }
 }
