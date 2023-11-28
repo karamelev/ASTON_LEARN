91406108c7f076b34d6ef744c3997b5b768f99d3
karamelev <karamelev@tut.by>
2023-11-28 11:04:33 +0300

    Lesson_12_Junit

--- ASTON_LEARN_Junit/src/test/java/TestFactorialCalculator.java
+++ ASTON_LEARN_Junit/src/test/java/TestFactorialCalculator.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,22 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestFactorialCalculator {
-        FactorialCalculator calculator = new FactorialCalculator();
+    FactorialCalculator calculator = new FactorialCalculator();
 
     @ParameterizedTest
-    @CsvSource (value = {
+    @CsvSource(value = {
             "0,1",
             "1,1",
             "5,120",
-            "10,3628800"
+            "-10,3628800"
     })
-    public void testFactorialCalculator (int input, int result) {
-        Assertions.assertEquals(result,calculator.factorialCalculator(input));
-    }
-
-    @Test()
-    public void testFactorialCalculatorForNegativeNumber () {
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> calculator.factorialCalculator(-5));
-        assertEquals(exception.getMessage(),"Факториал отрицательного числа не определен");
+    public void testFactorialCalculator(int input, int result) {
+        if (input > 0) {
+            Assertions.assertEquals(result, calculator.factorialCalculator(input));
+        } else {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculator.factorialCalculator(-5));
+            assertEquals(exception.getMessage(), "Факториал отрицательного числа не определен");
+        }
     }
 }
+
+
